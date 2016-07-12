4450c7079435eca198e9bf37c6d127cd2457cd44
OH JONG HUN <ojh3636@kaist.ac.kr>
2016-07-13 01:22:09 +0900

    implemented loginactivity

--- app/src/main/java/com/project/cs496/weather_app/SettingActivity.java
+++ app/src/main/java/com/project/cs496/weather_app/SettingActivity.java
@@ -1,7 +1,18 @@
 package com.project.cs496.weather_app;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by q on 2016-07-12.
  */
-public class SettingActivity {
+public class SettingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
 }
